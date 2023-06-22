82b7ea480f3a22013fd65b342cbdaddbb6434540
Brad Rutkowski <rutkowski.bradley@gmail.com>
2023-06-22 19:48:55 -0400

    Updated PointGeometry object for clarity and added TODO

--- src/main/java/com/rutkowski/gisspatialdatamanager/types/geometries/PointGeometry.java
+++ src/main/java/com/rutkowski/gisspatialdatamanager/types/geometries/PointGeometry.java
@@ -8,13 +8,19 @@ import lombok.Data;
 @Data
 public class PointGeometry {
  
-  private final double x;   // Latitude
-  private final double y;   // Longitude
+  private final double latitude;
+  private final double longitude;
 
+  /*
+   * These @JsonProprty() annotations were put in place to fix an issue with mapping ESRI's response
+   * back to our custom object. An unintended side effect is that the names "x" and "y" (which can be
+   * confusing) are sent back to our client.
+   * TODO: Update this so that "x" and "y" are returned to our client as "longitude" and "latitude", respectively
+   */
   @JsonCreator
-  public PointGeometry(@JsonProperty("x") double x, @JsonProperty("y") double y) {
-    this.x = x;
-    this.y = y;
+  public PointGeometry(@JsonProperty("x") double longitude, @JsonProperty("y") double latitude) {
+    this.latitude = latitude;
+    this.longitude = longitude;
   }
 
 }
