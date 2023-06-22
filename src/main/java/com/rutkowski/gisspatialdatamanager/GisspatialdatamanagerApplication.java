f468af8d20254b1f06fb07e8b683698bc86b178f
Brad Rutkowski <rutkowski.bradley@gmail.com>
2023-06-22 16:15:05 -0400

    Initial commit.

---
+++ src/main/java/com/rutkowski/gisspatialdatamanager/GisspatialdatamanagerApplication.java
@@ -0,0 +1,13 @@
+package com.rutkowski.gisspatialdatamanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GisspatialdatamanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GisspatialdatamanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rutkowski/gisspatialdatamanager/GisspatialdatamanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rutkowski.gisspatialdatamanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GisspatialdatamanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
